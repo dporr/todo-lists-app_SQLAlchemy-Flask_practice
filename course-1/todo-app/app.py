95e563c86068b24958ff7a03e87c19a12dd5a041
dporr <dporras.mail@gmail.com>
2020-05-03 13:22:37 -0600

    Add todo model

--- course-1/todo-app/app.py
+++ course-1/todo-app/app.py
@@ -10,19 +10,19 @@ user = os.getenv("POSTGRES_USER")
 password = os.getenv("POSTGRES_PASSWORD")
 host = os.getenv("POSTGRES_HOST")
 schema = os.getenv("APP_SCHEMA")
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgress://{user}:{password}@{host}:5432/{schema}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgress://{user}:{password}@{host}:5432/{schema}'
 db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key= True)
+    description = db.Column(db.String, nullable= False)
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template("index.html", data=[
-        {"description": "Item 1"},
-        {"description": "Second item"},
-        {"description": "A third one..."},
-        {"description": "4th Item"}
-        ]
-    )
+    return render_template("index.html", data=Todo.query.all())
 
 if __name__ == '__main__':
     app.run()
