892abf2103da5bd41fb1ec11568d63faa41ca789
dporr <dporras.mail@gmail.com>
2020-05-03 13:14:08 -0600

    Add db config

--- course-1/todo-app/app.py
+++ course-1/todo-app/app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+from sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
+#Here we better implement a sectrets manager, but by now this is OK.
+#TODO: Implement a hashicorp vault cluster un integrate this app with it
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+host = os.getenv("POSTGRES_HOST")
+schema = os.getenv("APP_SCHEMA")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgress://{user}:{password}@{host}:5432/{schema}'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
